90b33dfb5b28d1cf65fc3a785712bf23f7d64557
Pavel Bortnik <pavel_bortnik@epam.com>
2020-01-22 20:35:22 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,8 +8,6 @@ node {
 
            stage('Checkout'){
                 checkout scm
-                sh 'git checkout master'
-                sh 'git pull'
             }
 
             docker.withServer("$DOCKER_HOST") {
@@ -22,7 +20,7 @@ node {
                 }
 
                 stage('Deploy container') {
-                   sh "docker-compose -p reportportal5 -f $COMPOSE_FILE_RP_5 up -d --force-recreate index"
+                   sh "docker-compose -p reportportal -f $COMPOSE_FILE_RP up -d --force-recreate index"
                 }
             }
 
