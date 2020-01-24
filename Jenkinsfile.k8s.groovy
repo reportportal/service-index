3f132572b870115ffaf9d58b5e47a4b1fc4cb473
andrei_varabyeu <andrei_varabyeu@epam.com>
2020-01-24 16:19:39 +0300

    fix path to config file

--- Jenkinsfile.k8s.groovy
+++ Jenkinsfile.k8s.groovy
@@ -140,7 +140,7 @@ podTemplate(
                 println("Triggering build of SAST job: ${dastJobName}...")
                 build job: dastJobName,
                         parameters: [
-                                string(name: "CONFIG", value: "carrier/config.yaml"),
+                                string(name: "CONFIG", value: "$ciDir/rp/carrier/config.yaml"),
                                 string(name: "SUITE", value: "rpportal_dev_dast"),
                                 booleanParam(name: "DEBUG", defaultValue: false)
                         ],
