d636024c565b76a12b62608d2788b9a973362fd2
Hleb Kanonik <esscyh@gmail.com>
2022-06-08 14:49:13 +0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,48 +2,40 @@
 
 node {
 
-       load "$JENKINS_HOME/jobvars.env"
+        load "$JENKINS_HOME/jobvars.env"
 
-       dir('src/github.com/reportportal/service-index') {
+        dir('src/github.com/reportportal/service-index') {
 
-           stage('Checkout'){
+            stage('Checkout'){
                 checkout scm
             }
 
-            docker.withServer("$DOCKER_HOST") {
-                stage('Build Docker Image') {
-                   sh """          
-                   MAJOR_VER=\$(cat VERSION)
-                   BUILD_VER="\${MAJOR_VER}-${env.BUILD_NUMBER}"
-                   make build-image v=\$BUILD_VER
-                   """
-                }
-
-                stage('Deploy container') {
-                   sh "docker-compose -p reportportal -f $COMPOSE_FILE_RP up -d --force-recreate index"
-                   stage('Push to ECR') {
-                      withEnv(["AWS_URI=${AWS_URI}", "AWS_REGION=${AWS_REGION}"]) {
-                             sh 'docker tag reportportal-dev/service-index ${AWS_URI}/service-index'
-                             def image = env.AWS_URI + '/service-index'
-                             def url = 'https://' + env.AWS_URI
-                             def credentials = 'ecr:' + env.AWS_REGION + ':aws_credentials'
-                             docker.withRegistry(url, credentials) {
-                                docker.image(image).push('SNAPSHOT-${BUILD_NUMBER}')
-                             }
-                      }
-                   }              
-                }
-                   
-                stage('Cleanup') {
-                   docker.withServer("$DOCKER_HOST") {
-                       withEnv(["AWS_URI=${AWS_URI}"]) {
-                                  sh 'docker rmi ${AWS_URI}/service-index:SNAPSHOT-${BUILD_NUMBER}'
-                                  sh 'docker rmi ${AWS_URI}/service-index:latest'
-                              }
-                       }
-                }
+            
+            stage('Build Docker Image') {
+                sh """          
+                MAJOR_VER=\$(cat VERSION)
+                BUILD_VER="\${MAJOR_VER}-${env.BUILD_NUMBER}"
+                make build-image v=\$BUILD_VER
+                """
             }
 
+            stage('Push to registries') {
+                withEnv(["AWS_URI=${AWS_URI}", "AWS_REGION=${AWS_REGION}"]) {
+                    sh 'docker tag reportportal-dev/service-index ${AWS_URI}/service-index:SNAPSHOT-${BUILD_NUMBER}'
+                    def image = env.AWS_URI + '/service-index' + ':SNAPSHOT-' + env.BUILD_NUMBER
+                    def url = 'https://' + env.AWS_URI
+                    def credentials = 'ecr:' + env.AWS_REGION + ':aws_credentials'
+                    docker.withRegistry(url, credentials) {
+                        docker.image(image).push()
+                    }
+                }
+            }
+                     
+            stage('Cleanup') {
+                withEnv(["AWS_URI=${AWS_URI}"]) {
+                    sh 'docker rmi ${AWS_URI}/service-index:SNAPSHOT-${BUILD_NUMBER}'
+                    sh 'docker rmi reportportal-dev/service-index:latest'
+                }
+            }
         }
 }
-
