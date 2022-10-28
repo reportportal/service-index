ff625363cda158f7fc851402bc9754a6f908f547
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-10-28 16:47:20 +0300

    Add resource paths

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/reportportal/commons-go/v5/commons"
 	"github.com/reportportal/commons-go/v5/conf"
@@ -27,6 +26,7 @@ func main() {
 		TraefikV2Mode bool   `env:"TRAEFIK_V2_MODE" envDefault:"false"`
 		TraefikLbURL  string `env:"LB_URL" envDefault:"http://localhost:9091"`
 		LogLevel      string `env:"LOG_LEVEL" envDefault:"info"`
+		Path          string `env:"RESOURCE_PATH" envDefault:""`
 	}{
 		ServerConfig: cfg,
 	}
@@ -60,24 +60,24 @@ func main() {
 	srv.WithRouter(func(router *chi.Mux) {
 		router.Use(middleware.Logger)
 		router.NotFound(func(w http.ResponseWriter, rq *http.Request) {
-			http.Redirect(w, rq, "/ui/#notfound", http.StatusFound)
+			http.Redirect(w, rq, rpCfg.Path+"/ui/#notfound", http.StatusFound)
 		})
 
-		router.HandleFunc("/composite/info", func(w http.ResponseWriter, r *http.Request) {
+		router.HandleFunc(rpCfg.Path+"/composite/info", func(w http.ResponseWriter, r *http.Request) {
 			if err := server.WriteJSON(http.StatusOK, aggreg.AggregateInfo(), w); nil != err {
 				log.Error(err)
 			}
 		})
-		router.HandleFunc("/composite/health", func(w http.ResponseWriter, r *http.Request) {
+		router.HandleFunc(rpCfg.Path+"/composite/health", func(w http.ResponseWriter, r *http.Request) {
 			if err := server.WriteJSON(http.StatusOK, aggreg.AggregateHealth(), w); nil != err {
 				log.Error(err)
 			}
 		})
-		router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "/ui/", http.StatusFound)
+		router.HandleFunc(rpCfg.Path+"/", func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, rpCfg.Path+"/ui/", http.StatusFound)
 		})
-		router.HandleFunc("/ui", func(w http.ResponseWriter, r *http.Request) {
-			http.Redirect(w, r, "/ui/", http.StatusFound)
+		router.HandleFunc(rpCfg.Path+"/ui", func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, rpCfg.Path+"/ui/", http.StatusFound)
 		})
 	})
 	srv.StartServer()
